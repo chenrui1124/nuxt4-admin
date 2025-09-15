5da3c7a0d685f748144e0c46a49d28bbcb2bfccc
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-09-15 22:30:39 +0800

    Implement switch locale

---
+++ app/components/LocaleSwitcher.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import type { DropdownMenuItem } from '@nuxt/ui'
+
+const { locale, locales, setLocale } = useI18n()
+
+const items = computed(() =>
+  locales.value.map<DropdownMenuItem>(({ code, name }) => ({
+    label: name,
+    type: 'checkbox' as const,
+    checked: locale.value === code,
+    onSelect: async () => await setLocale(code),
+  })),
+)
+</script>
+
+<template>
+  <UDropdownMenu :content="{ align: 'end' }" :items>
+    <UButton icon="i-fluent:local-language-zi-24-filled" variant="ghost" class="ml-auto" />
+  </UDropdownMenu>
+</template>
--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -5,6 +5,7 @@
 <template>
   <div class="flex h-screen w-screen flex-col items-center justify-center">
     <header class="fixed inset-x-0 top-0 flex h-14 items-center gap-3 px-3">
+      <LocaleSwitcher />
       <ColorModeSwitcher />
     </header>
     <main class="contents">
