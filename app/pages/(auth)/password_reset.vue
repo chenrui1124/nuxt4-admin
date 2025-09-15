13ae547cfc03faa40c8f97f7c800a08aac263ce1
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-09-15 23:04:38 +0800

    Implement password reset page ui

---
+++ app/pages/(auth)/password_reset.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+import { z } from 'zod'
+
+const fields = computed(() => [
+  {
+    name: 'email',
+    type: 'text' as const,
+    icon: 'i-fluent:mail-24-filled',
+    label: $t('auth.email'),
+    placeholder: 'example@example.com',
+    required: true,
+  },
+])
+
+const schema = z.object({
+  email: z.email($t('auth.email_invalid')).min(1, $t('auth.email_invalid')),
+})
+
+type Schema = z.output<typeof schema>
+
+function onSubmitSendEmail(_payload: FormSubmitEvent<Schema>) {
+  //
+}
+</script>
+
+<template>
+  <NuxtLayout name="auth">
+    <UAuthForm
+      @submit="onSubmitSendEmail"
+      :fields
+      :schema
+      icon="i-fluent:password-reset-48-filled"
+      loading-auto
+      :submit="{ label: $t('auth.send_email') }"
+      :title="$t('auth.password_reset')"
+      class="max-h-11/12 w-sm max-w-11/12"
+      #footer
+    >
+      <UButton to="/login" block color="neutral" variant="subtle">
+        {{ $t('auth.return') }}
+      </UButton>
+    </UAuthForm>
+  </NuxtLayout>
+</template>
