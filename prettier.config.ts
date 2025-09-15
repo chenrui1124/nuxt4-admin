964df138f5484421affd86cfc5382481a821dda7
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-09-15 22:15:28 +0800

    Prettier setup

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'prettier'
+
+export default {
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './app/app.css',
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+} satisfies Config
