bb8d2fb8540bfc56fee619f49b486feb7e76d510
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-09-15 22:05:43 +0800

    Add module: @nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // Nuxt
   compatibilityDate: '2025-07-15',
+  css: ['~/app.css'],
   devtools: { enabled: true },
+
+  // Modules
+  modules: ['@nuxt/ui'],
+
+  // @nuxt/ui
+  ui: {
+    fonts: false,
+  },
 })
