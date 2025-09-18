401b47878360864160fb25a57b98fcf1af12d511
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-09-18 22:10:28 +0800

    Update auth layout styles

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex h-screen w-screen flex-col items-center justify-center">
+  <div
+    style="height: 100vh; height: 100dvh"
+    class="flex w-screen flex-col items-center justify-center pb-14"
+  >
     <header class="fixed inset-x-0 top-0 flex h-14 items-center gap-3 px-3">
       <LocaleSwitcher />
       <ColorModeSwitcher />
