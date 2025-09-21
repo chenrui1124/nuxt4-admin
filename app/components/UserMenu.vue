15d0e964b1399afa73aba83cd536ce218322d7f7
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-09-21 20:38:21 +0800

    Fix UserMenu component type error

--- app/components/UserMenu.vue
+++ app/components/UserMenu.vue
@@ -24,15 +24,15 @@ const items = computed<DropdownMenuItem[]>(() => [
 </script>
 
 <template>
-  <UDropdownMenu :content="{ align: 'end' }" :items>
-    <UAvatar :alt="user?.name" size="xs" :src="user?.avatar" class="m-1" />
+  <UDropdownMenu v-if="user" :content="{ align: 'end' }" :items>
+    <UAvatar :alt="user.name" size="xs" :src="user.avatar!" class="m-1" />
     <template #content-top>
       <div
         class="m-1 -mb-1 grid grid-cols-[min-content_1fr] grid-rows-2 items-center gap-x-1 pr-1.5 text-sm"
       >
-        <UAvatar :alt="user?.name" :src="user?.avatar" class="col-start-1 row-span-2 m-1.5" />
-        <div class="col-start-2">{{ user?.name }}</div>
-        <div class="col-start-2 text-default/75">{{ user?.email }}</div>
+        <UAvatar :alt="user.name" :src="user.avatar!" class="col-start-1 row-span-2 m-1.5" />
+        <div class="col-start-2">{{ user.name }}</div>
+        <div class="col-start-2 text-default/75">{{ user.email }}</div>
       </div>
     </template>
   </UDropdownMenu>
